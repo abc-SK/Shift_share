2ea3e701b015c20cf8004e91cc92806a57403321
Kazuya Shoji <kazuya.ab11@gmail.com>
2026-05-14 13:55:48 +0900

    Configure Firebase project

--- firebase-config.js
+++ firebase-config.js
@@ -1,8 +1,10 @@
 export const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: "",
-};
+    apiKey: "AIzaSyD1SQ4nZYLmk7HPzMG2CPlEQ7o9eAbDyIE",
+    authDomain: "shift-share-ecb1d.firebaseapp.com",
+    projectId: "shift-share-ecb1d",
+    storageBucket: "shift-share-ecb1d.firebasestorage.app",
+    messagingSenderId: "164444873395",
+    appId: "1:164444873395:web:08b19e01f4e0b204ea46c6",
+    measurementId: "G-3BYYZ1KLNT"
+  };
+
